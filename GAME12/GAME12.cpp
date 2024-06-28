fd02aa8dcce89ea23ab5001832a78a5083357ab7
大希 栁田 <taiki2950@iCloud.com>
2024-06-28 15:24:40 +0900

    完成品

--- GAME12/GAME12.cpp
+++ GAME12/GAME12.cpp
@@ -1,6 +1,7 @@
-#include "../../libOne/inc/libOne.h"
-#include "../MAIN/MAIN.h"
-#include "GAME12.h"
+#include"../../libOne/inc/libOne.h"
+#include"../MAIN/MAIN.h"
+#include"GAME12.h"
+
 namespace GAME12
 {
 	int GAME::create()
@@ -15,14 +16,191 @@ namespace GAME12
 
 	void GAME::proc()
 	{
-		clear(0, 0, 64);
-		textSize(50);
-		fill(255, 255, 0);
-		text("GAME12", 0, 100);
-		fill(255);
-		text("ENTERキーでメニューに戻る", 0, 1080);
+
+		//シーン切り替え
+		if (State == TITLE)title();
+		else if (State == INIT)init();
+		else if (State == PLAY)play();
+		else if (State == WAIT)wait();
+		else if (State == OVER)over();
+		else if (State == END)ending();
+	}
+
+	void GAME::count() {
+		for (int i = 0; i < Qnumber;i++) {
+				str = strlen(ward[i][1]);
+				sum += str;
+			}
+
+	}
+	
+	INPUT_CODE GAME::gettriggerkeycode() {
+		for (int i = (int)KEY_A; i <= (int)KEY_ENTER; i++) {
+			if (isTrigger((INPUT_CODE)i)) {
+				return (INPUT_CODE)i;
+			}
+		}
+		return (INPUT_CODE)0;
+	}
+	
+		
+	void GAME::title(){
+		//描画--------------------------------------------------
+		clear(60);
+		//テキスト情報
+		fill(0, 255, 0);
+		textSize(100);
+		text("タイピングゲーム", 600, 450);
+		fill(0, 0, 0);
+		textSize(40);
+		text("クリックでゲームスタート", 750, 500);
+		text("Enterでメニューに戻る", 750, 550);
+		//シーン切り替え-----------------------------------------
+		if (isTrigger(MOUSE_LBUTTON)) {
+			//次のシーケンス
+			State = INIT;
+			return;
+		}
+		//メニューに戻る-----------------------------------------
 		if (isTrigger(KEY_ENTER)) {
 			main()->backToMenu();
 		}
 	}
+
+	void GAME::init() {
+		clear(60, 60, 60);
+		miss = 0;
+		num = 0;
+		wnum = 0;
+		flag = 0;
+		fflag = 0;
+		gend = 0;
+		lend = 0;
+		fill(0, 0, 0);
+		textSize(60);
+		text("表示された文字をタイピングしよう", 525, 500);
+		text("左クリックでゲームスタート", 575, 550);
+		
+		if (isTrigger(MOUSE_LBUTTON)) {
+			count();
+			State = WAIT;
+			return;
+		}
+	}
+	void GAME::wait() {
+		clear(200);
+		textSize(60);
+		text("スペースキーで開始", 525, 500);
+		
+		if (isTrigger(KEY_SPACE)) {
+			clear(200);
+			State = PLAY;
+			return;
+		}
+	}
+
+	void GAME::draw(int snum) {
+		fill(0);
+		textSize(200);
+		text((let) +ward[snum][0], 550, 300);
+		text((let) +ward[snum][1], 550, 500);
+		print(curtime);
+		char str[128];
+		strcpy_s(str, sizeof(str), ward[num][1]);
+		str[wnum] = '\0';
+		fill(255, 0, 0);
+		textSize(200);
+		text(str, 550, 500);
+	}
+
+	void GAME::dec(int num, int wnum) {
+		
+			if (ch[num][wnum] - 1 == ward[num][1][wnum] - 'a') {
+				
+				flag++;
+			}
+			else if (ch[num][wnum] - 1 != ward[num][1][wnum] - 'a') {
+				fflag++;
+			}
+			
+	}
+	
+
+	void GAME::play() {
+		clear(200);
+		
+		curtime += delta;
+		size_t len = strlen(ward[num][1]);
+		draw(num);	
+		ch[num][wnum] = gettriggerkeycode();
+		if (ch[num][wnum] != 0) {
+			dec(num, wnum);
+		}
+		if (fflag == 1) {
+			fflag = 0;
+			miss++;
+			fill(0);
+			textSize(300);
+			text("×", 700, 550);
+		}
+
+		if (flag == 1) {
+			wnum++;
+			lend++;
+			flag = 0;
+		}
+
+		if (lend == len) {
+			num++;
+			wnum = 0;
+			lend = 0;
+			gend++;
+			clear(200);
+		}
+
+			
+		if(gend == Qnumber){
+			State = OVER;
+		}
+				
+
+			
+			
+	}
+	void GAME::over() {
+		clear(200);
+		fill(0);
+		textSize(60);
+		text("終了", 900, 550);
+		text("左クリックで結果を見る", 600, 600);
+		if(isTrigger(MOUSE_LBUTTON)) {
+			State = END;
+			return;
+		}
+	}
+	
+	void GAME::ending() {
+		clear(0, 255, 0);
+		//ゲームオーバーテキスト 
+		fill(0, 0, 255);
+		textSize(80);
+		text((let)"一秒あたりのタイプ数:" +sum / curtime, 550, 100);
+		text((let)"タイム:" +curtime, 550, 200);
+		text((let)"ミスタイプ:"+miss, 550, 300);
+		text("GAMEOVER", 650, 450);
+		text("SPACEキーでタイトルに戻る", 500, 550);
+		if (isTrigger(KEY_SPACE)) {
+			State = TITLE;
+		}
+	}
+
+	void GAME::gmain() {
+		window( 1920,1080,full);
+	}
+
+
+
+
 }
+
+
--- GAME12/GAME12.h
+++ GAME12/GAME12.h
@@ -1,5 +1,17 @@
 #pragma once
 #include "../MAIN/GAME_BASE.h"
+#include<time.h>
+#include<conio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdio.h>
+#include<iostream>
+#include<chrono>
+#include<thread>
+#include<random>
+#include"../../libOne/inc/input.h"
+#define Qnumber 14
+
 namespace GAME12
 {
     class GAME :
@@ -11,5 +23,46 @@ namespace GAME12
         int create();
         void proc();
         void destroy();
+    private:
+        enum STATE { TITLE, INIT, WAIT, PLAY, OVER ,END };
+        STATE State = TITLE;
+        void title();
+        void init();
+        void wait();
+        void play();
+        void over();
+        void ending();
+        void count();
+        void gmain();
+        void dec(int num, int wnum);
+        void draw(int snum);
+        INPUT_CODE gettriggerkeycode();
+
+        int miss, num, wnum, flag, fflag, gend, lend;
+        INPUT_CODE ch[Qnumber][100];
+        size_t sum;
+        size_t str;
+        float curtime;
+       
+
+        const char* ward[Qnumber][2] = {
+            {"‚È‚µ","nasi"},
+            {"‚Î‚È‚È","banana"},
+            {"‚è‚ñ‚²","ringo"},
+            {"‚¢‚¿‚²","itigo"},
+            {"‚Ô‚Ç‚¤","budou"},
+            {"‚«‚¤‚¢","kiui"},
+            {"‚´‚­‚ë","zakuro"},
+            {"‚·‚¢‚©","suika"},
+            {"‚ê‚à‚ñ","remon"},
+            {"‚ç‚¢‚¿","raiti"},
+            {"‚Ç‚è‚ ‚ñ","dorian"},
+            {"‚¨‚ê‚ñ‚¶","orenji"},
+            {"‚³‚­‚ç‚ñ‚Ú","sakuranbo"},
+            {"‚Ï‚¢‚È‚Á‚Õ‚é","painappuru"},
+            
+                       
+        };
+
     };
 }
